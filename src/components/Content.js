fee5fce4fd9ba5e3d0ba4bdd12947f0c97fd3e44
Verlic <hernan.meydac@auth0.com>
2018-11-30 22:07:14 -0300

    Update title to allow changes

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,30 +1,38 @@
 import React, { Component } from "react";
 import Title from "./Title.js";
 import Body from "./Body.js";
-import "./Content.css"
+import "./Content.css";
 
 import Api from "../api";
 
 const client = new Api();
 
-
 class Content extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { note: client.getNote(this.props.match.params.note) };
+  }
+
+  onTitleChange(value) {
+    this.setState({note:{title:value}});
+  }
 
   render() {
-    var note = client.getNote(this.props.match.params.note); 
-
-    if (!note) {
+    if (!this.state.note) {
       return (
         <div id="content">
-        <h2>404 - Note not found.... che</h2>
+          <h2>404 - Note not found.... che</h2>
         </div>
-      )
+      );
     }
 
     return (
       <div id="content">
-        <Title text={note.title} />
-        <Body text={note.body}/>
+        <Title
+          text={this.state.note.title}
+          onTextChange={(value) => this.onTitleChange(value)}
+        />
+        <Body text={this.state.note.body} />
       </div>
     );
   }
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,8 +2,42 @@ import React, { Component } from "react";
 import "./Title.css";
 
 class Title extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isEditing: false };
+  }
+
+  edit(event) {
+    this.setState({ isEditing: true });
+  }
+
+  handleChange(value) {
+    this.props.onTextChange(value);
+  }
+
+  handleKeyPress(e) {
+    if (e.key === "Enter") {
+      this.setState({ isEditing: false });
+    }
+  }
+
   render() {
-    return <h1 className="title" >{this.props.text}</h1>;
+    if (!this.state.isEditing) {
+      return (
+        <h1 className="title" onClick={event => this.edit(event)}>
+          {this.props.text}
+        </h1>
+      );
+    } else {
+      return (
+        <input
+          className="title-input"
+          onChange={event => this.handleChange(event.target.value)}
+          value={this.props.text}
+          onKeyPress={event => this.handleKeyPress(event)}
+        />
+      );
+    }
   }
 }
 
